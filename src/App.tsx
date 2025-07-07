c0832ffe84168f298928bcd50b9469492cf64606
Ashish Raut <theashish32@gmail.com>
2025-07-07 18:31:44 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import { encoding_for_model } from "@dqbd/tiktoken";
 function App() {
     const modelList = [
         // GPT-4 Series
-        { name: "GPT-4o", value: "gpt-4o" }, 
-        { name: "GPT-4 Turbo", value: "gpt-4-turbo" }, 
+        { name: "GPT-4o", value: "gpt-4o" },
+        { name: "GPT-4 Turbo", value: "gpt-4-turbo" },
         { name: "GPT-4", value: "gpt-4" }, // 
 
         // GPT-3.5 Series
         { name: "GPT-3.5 Turbo", value: "gpt-3.5-turbo" },
         { name: "GPT-3.5 Turbo-0125", value: "gpt-3.5-turbo-0125" },
         { name: "GPT-3.5 Turbo-1106", value: "gpt-3.5-turbo-1106" },
-        { name: "GPT-3.5 Turbo-16k", value: "gpt-3.5-turbo-16k" }, 
+        { name: "GPT-3.5 Turbo-16k", value: "gpt-3.5-turbo-16k" },
 
         // GPT-3 Text Completion Models
         { name: "Text-Davinci-003", value: "text-davinci-003" },
@@ -23,9 +23,9 @@ function App() {
         { name: "Text-Ada-001", value: "text-ada-001" },
 
         // Code Completion Models
-        { name: "Code-Davinci-002", value: "code-davinci-002" }, 
-        { name: "Code-Cushman-001", value: "code-cushman-001" }, 
-      ];
+        { name: "Code-Davinci-002", value: "code-davinci-002" },
+        { name: "Code-Cushman-001", value: "code-cushman-001" },
+    ];
 
     const [darkmode, setDarkmode] = useState(false);
     const [model, setModel] = useState(false);
@@ -38,23 +38,23 @@ function App() {
     const [nums, setNums] = useState<number[]>([]);
 
     useEffect(() => {
-        function fun(modelname: any){
+        function fun(modelname: any) {
             const encoder = encoding_for_model(modelname);
             const tokens = encoder.encode(input);
             setNums(Array.from(tokens));
             setTotaltokens(tokens.length);
 
             const arr: string[] = [];
-            const textDecoder = new TextDecoder(); 
+            const textDecoder = new TextDecoder();
 
             for (let i = 0; i < tokens.length; i++) {
                 const tokenId = tokens[i];
                 const tokenBytes = encoder.decode(new Uint32Array([tokenId]));
-                const tokenStr = textDecoder.decode(tokenBytes); 
-                arr.push(tokenStr); 
+                const tokenStr = textDecoder.decode(tokenBytes);
+                arr.push(tokenStr);
             }
 
-             setDecoded(arr);
+            setDecoded(arr);
         }
         fun(selectedModelBackend);
     }, [input, selectedModelBackend]);
@@ -82,9 +82,9 @@ function App() {
                             </svg>
 
                         ) : (
-                                <svg onClick={() => setDarkmode(!darkmode)} className="cursor-pointer size-6 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                                </svg>
+                            <svg onClick={() => setDarkmode(!darkmode)} className="cursor-pointer size-6 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                            </svg>
                         )
                     }
                 </div>
@@ -100,12 +100,21 @@ function App() {
                 <div className={`h-full w-full md:w-1/2`}>
                     <div className={`text-xl font-montserrat font-semibold mb-3 ${darkmode ? "text-white" : "text-black"}`}>Token count - {totaltokens}</div>
                     <div className="w-full flex flex-col gap-4">
-                        <div style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }} className={`${darkmode ? "text-white" : "text-black"} px-3 py-2 font-mono h-[280px] w-full rounded-lg border-[2px] ${darkmode ? "border-neutral-700/70" : "border-gray-300"} `}>
+                        <div style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }} className={`${darkmode ? "text-white" : "text-black"} overflow-scroll px-3 py-2 font-mono h-[280px] w-full rounded-lg border-[2px] ${darkmode ? "border-neutral-700/70" : "border-gray-300"} `}>
                             {decoded.map((e, i) => (
-                                <span key={i} className="px-2 font-mono">{e}</span>
+                                <span key={i} className="px-1 font-mono">{e}</span>
                             ))}
                         </div>
-                        <div style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }} className={`${darkmode ? "text-white" : "text-black"} px-2 py-3 h-[280px] w-full rounded-lg border-[2px] ${darkmode ? "border-neutral-700/70" : "border-gray-300"} `}>
+                        <div
+                            style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}
+                            className={`${darkmode ? "text-white" : "text-black"} 
+                            overflow-scroll 
+                            px-2 py-3 
+                            h-[280px] w-full 
+                            rounded-lg border-[2px] 
+                            ${darkmode ? "border-neutral-700/70" : "border-gray-300"} 
+                            flex flex-wrap`}
+                        >
                             {nums.map((e, i) => (
                                 <span key={i} className="px-2 font-mono">{e}</span>
                             ))}
